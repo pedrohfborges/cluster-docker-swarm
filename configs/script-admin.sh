b7a992fe2a37d8f7cea7c528dd7f9ce01f713e53
pedrohfborges <111831621+pedrohfborges@users.noreply.github.com>
2023-09-21 01:37:59 -0300

    Update script-admin.sh

--- configs/script-admin.sh
+++ configs/script-admin.sh
@@ -17,6 +17,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo chmod 777 /var/run/docker.sock
 
+docker swarm init --advertise-addr 192.168.56.0 > /configs/token.txt
 
-
-
+echo '#!bin/bash' > /configs/script.sh
+cat /configs/token.txt | grep -v ^- /configs/token.txt | grep 7$ | cut -d " " -f5-10 >> /configs/script.sh
